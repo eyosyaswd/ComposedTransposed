3edf7879f68238f367804fa51bba8180856865f3
Eyosyas Dagnachew <eyosyaswd@outlook.com>
2017-02-12 12:47:00 -0500

    Initial Commit

---
+++ ComposedTransposed/ComposedTransposed.py
@@ -0,0 +1,33 @@
+# ComposedTransposed
+# Authors: Eyosyas Dagnachew, Rozda Askari, Dima Shmagin
+
+# 1. open the file to read
+# 2. Read the file one character at a time
+        # if its a charater not a space or endline
+                # read the next character
+                    # if that charcater is not a space or endline
+                        # concatenate the two character and put them in an array
+                    # else put first chactaer in array based on cases
+        # else save
+
+
+def main():
+
+    musical_scale = {0: 'A', }
+
+
+
+    input_file = open("input.txt", "r")
+    #print(input_file.read())
+    music = []
+    with input_file as file:
+
+        for line in file:
+            for word in line.split():
+                print(word)
+        #    line = line.strip()
+        #    music.append(line)
+    print(music)
+
+
+main()
